30fabae8227401eb51e353323a23ccc70fd88420
hilton-j <lhmajola@outlook.com>
2024-07-16 13:42:07 +0200

    13:41 New Update

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,22 @@
 import './footer.css'
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaPinterest } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div>footer</div>
+        <footer className='footer-section'>
+            <div className='socials-footer'>
+                <a href="https://facebook.com" target="_blank"><FaFacebook /></a>
+                <a href="https://facebook.com" target="_blank"><FaLinkedin /></a>
+                <a href="https://facebook.com" target="_blank"><FaPinterest /></a>
+            </div>
+            <div className='socials-btn'>
+                <a href='#about'>ABOUT</a>
+                <a href='#articles'>ARTICLES</a>
+                <button className='subscribe-btn'>SUBSCRIBE</button>
+            </div>
+        </footer>
     )
 }
 
