cd07dffa951ffe7c8ef812af870f5dbd6e7be1bb
hilton-j <lhmajola@outlook.com>
2024-07-17 11:54:31 +0200

    Subscribe 2024/07/17

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,16 +3,18 @@ import './header.css'
 const Header = () => {
     return (
         <section className='header-section container'>
-            <nav>
-                <div className='header-logo'><span >D</span><span className='primary-color'>Z</span></div>
-                <div className='header-link'>
-                    <a href='#about'>ABOUT</a>
-                    <a href='#articles'>ARTICLES</a>
-                </div>
-                <div className='header-subscribe'>
-                    <button className='subscribe-btn'>SUBSCRIBE</button>
-                </div>
-            </nav>
+            <section className='nav-section'>
+                <nav>
+                    <div className='header-logo'><span >D</span><span className='primary-color'>Z</span></div>
+                    <div className='header-link'>
+                        <a href='#about'>ABOUT</a>
+                        <a href='#articles'>ARTICLES</a>
+                    </div>
+                    <div className='header-subscribe'>
+                        <button className='subscribe-btn'>SUBSCRIBE</button>
+                    </div>
+                </nav>
+            </section>
             <section className='header-text'>
                 <h1>DRONOTICZ</h1>
                 <h2>THE ERA OF DRONES</h2>
--- src/components/subscribe/subscribe.jsx
+++ src/components/subscribe/subscribe.jsx
@@ -3,10 +3,15 @@ import './subscribe.css'
 const Subscribe = () => {
     return (
         <div className='subscribe-section' id='subscribe'>
-            <h2>SUBSCRIBE</h2>
-            <p>Sign-up to our newletter</p>
-            <input type='email' placeholder='Your email' required />
-            <button type='submit'>SUBMIT</button>
+            <div className='subscribe-display'>
+                <h2>SUBSCRIBE</h2>
+                <p>Sign-up to our newletter</p>
+                <form className='subscribe-form'>
+                    <input type='email' placeholder='Your email' required />
+                    <button type='submit'>SUBMIT</button>
+                </form>
+            </div>
+
         </div>
     )
 }
