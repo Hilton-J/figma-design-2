c68452eb6cec06e414327efcf4adcb2fbed72827
hilton-j <lhmajola@outlook.com>
2024-07-18 09:55:51 +0200

    09:55 2024/07/18

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -7,6 +7,7 @@ import './cards.css'
 const cards = () => {
     return (
         <div className='card-articles'>
+
             <div className='cards'>
                 <div className='card-image'>
                     <img src={Drone1} alt='Drone1' />
@@ -16,6 +17,7 @@ const cards = () => {
                     <div className='dparagraph'><p>How to automate surveillance drones</p></div>
                 </div>
             </div>
+
             <div className='cards'>
                 <div className='card-image'><img src={Drone2} alt='Drone2' /></div>
                 <div className='card-text'>
@@ -23,15 +25,17 @@ const cards = () => {
                     <div className='dparagraph'><p>Robotics and artificial Intelligence</p></div>
                 </div>
             </div>
+
             <div className='cards'>
-                <div className='card-image'><img src={Drone6} alt='Drone6' /></div>
+                <div className='card-image'><img src={Drone4} alt='Drone6' /></div>
                 <div className='card-text'>
                     <div className='bar'></div>
                     <div className='dparagraph'><p>Improving Lives through Robotics Prosthetics</p></div>
                 </div>
             </div>
+
             <div className='cards'>
-                <div className='card-image'><img src={Drone4} alt='Drone4' /></div>
+                <div className='card-image'><img src={Drone6} alt='Drone4' /></div>
                 <div className='card-text'>
                     <div className='bar'></div>
                     <div className='dparagraph'><p>Power of Robotics in Agriculture</p></div>
