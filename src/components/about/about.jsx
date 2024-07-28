6bc7a04af09baa950c91f240c0d9eaef125870c5
Hilton-J <lhmajola@outlook.com>
2024-07-28 14:28:16 +0200

    Data

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -5,10 +5,10 @@ const About = () => {
     return (
         <section id='about' className='about-section'>
             <div className='about-wrap'>
-                <section className='about-image'>
+                <div className='about-image'>
 
-                </section>
-                <section className='about-text'>
+                </div>
+                <div className='about-text'>
 
                     <h2>ABOUT US</h2>
                     <div className='bar'></div>
@@ -16,7 +16,7 @@ const About = () => {
                         Welcome to our dronoticz, where innovation and technology converge to shape the future. We revolutionize industries and improve lives worldwide through our passion for pushing the boundaries of what robots can do. With expertise in AI, computer vision, mechanical engineering, and automation, we lead the way in the robotics revolution. Our brilliant team creates intelligent machines that seamlessly integrate into daily life, enhancing productivity, safety, and efficiency.Join us on this exciting journey as we unlock new frontiers, solve complex challenges, and shape a future where intelligent machines work hand-in-hand with humans. Discover the power of robotics and be part of the revolution.
                     </p>
 
-                </section>
+                </div>
             </div>
         </section>
     )
--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -6,33 +6,35 @@ import './cards.css'
 
 const cards = () => {
     return (
-        <div className='card-articles'>
+        <div className='wrapper'>
+            <div className='card-articles'>
 
-            <article className='cards'>
-                <div className='card-image'>
-                    <img src={Drone1} alt='Drone1' />
-                </div>
-                <div className='bar'></div>
-                <div className='dparagraph'><p>How to automate surveillance drones</p></div>
-            </article>
+                <article className='cards'>
+                    <div className='card-image'>
+                        <img src={Drone1} alt='Drone1' />
+                    </div>
+                    <div className='bar'></div>
+                    <div className='dparagraph'><p>How to automate surveillance drones</p></div>
+                </article>
 
-            <article className='cards'>
-                <div className='card-image'><img src={Drone2} alt='Drone2' /></div>
-                <div className='bar'></div>
-                <div className='dparagraph'><p>Robotics and artificial Intelligence</p></div>
-            </article>
+                <article className='cards'>
+                    <div className='card-image'><img src={Drone2} alt='Drone2' /></div>
+                    <div className='bar'></div>
+                    <div className='dparagraph'><p>Robotics and artificial Intelligence</p></div>
+                </article>
 
-            <article className='cards'>
-                <div className='card-image'><img src={Drone4} alt='Drone6' /></div>
-                <div className='bar'></div>
-                <div className='dparagraph'><p>Improving Lives through Robotics Prosthetics</p></div>
-            </article>
+                <article className='cards'>
+                    <div className='card-image'><img src={Drone4} alt='Drone6' /></div>
+                    <div className='bar'></div>
+                    <div className='dparagraph'><p>Improving Lives through Robotics Prosthetics</p></div>
+                </article>
 
-            <article className='cards'>
-                <div className='card-image'><img src={Drone6} alt='Drone4' /></div>
-                <div className='bar'></div>
-                <div className='dparagraph'><p>Power of Robotics in Agriculture</p></div>
-            </article>
+                <article className='cards'>
+                    <div className='card-image'><img src={Drone6} alt='Drone4' /></div>
+                    <div className='bar'></div>
+                    <div className='dparagraph'><p>Power of Robotics in Agriculture</p></div>
+                </article>
+            </div>
         </div>
     )
 }
